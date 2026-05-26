6c17f121c65ddfad12f6941340a679354e72d5be
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-26 23:25:46 +0100

    feat: implement tf-atom-launch-template-aws module (#1)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the launch template"
+  value       = try(aws_launch_template.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the launch template"
+  value       = try(aws_launch_template.this[0].arn, null)
+}
+
+output "latest_version" {
+  description = "Latest version number"
+  value       = try(aws_launch_template.this[0].latest_version, null)
 }
